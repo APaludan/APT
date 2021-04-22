19c0a312e784e4d15c3fde6add450d0de4691830
APaludan <andreashp@outlook.com>
2021-04-22 15:01:27 +0200

    rydder endnu mere op

--- main.c
+++ main.c
@@ -16,14 +16,13 @@ double sf = 44000.0; // sampling frequency
 double bd = 200 * 2; // duration of each bit
 double freq = 440.0; // frequency of sine wave
 
-
 int main(void)
 {
     int mode = 0; // No webcam = 0 | webcam = 1
     cleanUp(mode);
     if (mode == 1)
     {
-        system("ffmpeg.exe -f dshow -y -i \"video=Lenovo EasyCamera\" -frames:v 1 underwater.png");
+        system("ffmpeg.exe -hide_banner -loglevel error -f  dshow -y -i \"video=Lenovo EasyCamera\" -frames:v 1 underwater.png");
         Sleep(1000);
     }
     ccImage();
@@ -62,6 +61,7 @@ int main(void)
 
     makeAudioBuffer(buf, input, filelen);
     makeAudio(buf, N);
+    printf("Transmitting...");
     system("out.wav");
     printf("Transmission finished\n");
     return 0;
@@ -78,7 +78,7 @@ void cleanUp(int idk)
 
 void ccImage(void)
 {
-    system("ffmpeg.exe -i underwater.png -q:v 5 -vf scale=360:-1 compressed.jpeg");
+    system("ffmpeg.exe -hide_banner -loglevel error -i underwater.png -q:v 5 -vf scale=360:-1 compressed.jpeg");
     system("img2bin.exe compressed.jpeg");
     printf("Image converted to bits.\n");
     return;
@@ -87,10 +87,9 @@ void ccImage(void)
 void makeAudio(int16_t *buf, long long int N)
 {
     // Pipe the audio data to ffmpeg, which writes it to a wav file
-    FILE *pipeout = popen("ffmpeg.exe -y -f s16le -ar 44000 -ac 1 -i - out.wav", "w");
+    FILE *pipeout = popen("ffmpeg.exe -hide_banner -loglevel error -y -f s16le -ar 44000 -ac 1 -i - out.wav", "w");
     fwrite(buf, 2, N, pipeout);
     pclose(pipeout);
-    printf("pt2 %d\n", buf[1285]);
 }
 
 void makeAudioBuffer(int16_t *buf, char *input, long long int filelen)
@@ -98,6 +97,7 @@ void makeAudioBuffer(int16_t *buf, char *input, long long int filelen)
     long long int n = 0; // buffer index
     int j = 0;           // bit array index
 
+    printf("Making audio buffer...");
     while (j < filelen) // loop - input
     {
         int e = n + bd;
@@ -125,7 +125,7 @@ void makeAudioBuffer(int16_t *buf, char *input, long long int filelen)
         }
     }
 
-    printf("Audio buffer finished");
+    printf("Audio buffer finished\n");
     return;
 }
 
@@ -133,8 +133,5 @@ void printInfo(long long int N, char *input)
 {
     float seconds = N / sf;
     float bps = strlen(input) / seconds;
-    printf("Input len: %d\n", strlen(input));
-    printf("N: %lld\n", N);
-    printf("seconds: %f\n", seconds);
-    printf("bps: %f\n", bps);
+    printf("Input len: %d\nN: %lld\nseconds: %f\nbps: %f\n", strlen(input), N, seconds, bps);
 }
