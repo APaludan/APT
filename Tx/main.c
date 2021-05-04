0ae877c3675a1030fedea5874c2e702f6aed156a
APaludan <andreashp@outlook.com>
2021-05-04 11:50:13 +0200

    super duper performance

--- Tx/main.c
+++ Tx/main.c
@@ -6,16 +6,16 @@
 #include <windows.h>
 #include <time.h>
 
-void printInfo(long long int N, int filelen);
-void makeAudioBuffer(int16_t *buf, char *input, long long int filelen);
-void makeAudio(int16_t *buf, long long int N);
+void printInfo(long int N, int filelen);
+void makeAudioBuffer(int16_t *buf, char *input, long int filelen);
+void makeAudio(int16_t *buf, long int N);
 void cleanUp(int mode);
 void ccImage(void);
-int16_t calcSample(int n, int sub, double p2dsf);
+void calcSamples(int16_t **samples);
 
 double amp = INT16_MAX - 1; // amplitude
 double sf = 88000.0;        // sampling frequency
-int bd = 400*5;             // duration of each bit (samples per bit)
+int bd = 400 * 5;           // duration of each bit (samples per bit)
 double freq = 880.0;        // frequency of sine wave
 
 int main(void)
@@ -39,9 +39,9 @@ int main(void)
             return 1;
         }
     }
-    fseek(fp, 0, SEEK_END);            // filepointer to end of file
-    long long int filelen = ftell(fp); // read file length
-    rewind(fp);                        // fp back to beginning
+    fseek(fp, 0, SEEK_END);       // filepointer to end of file
+    long int filelen = ftell(fp); // read file length
+    rewind(fp);                   // fp back to beginning
     char *input = malloc(filelen * sizeof(char));
     if (input == NULL)
     {
@@ -51,7 +51,7 @@ int main(void)
     fread(input, filelen, 1, fp); // read binary file
     fclose(fp);
 
-    long long int N = filelen * bd / 3.8; // number of samples
+    long int N = filelen * bd / 3.8; // number of samples
     printInfo(N, filelen);
 
     int16_t *buf = malloc((N + 1) * sizeof(int16_t)); // buffer
@@ -86,13 +86,13 @@ void cleanUp(int mode) // remove old files
 
 void ccImage(void) // compress and convert to bits
 {
-    system("ffmpeg.exe -hide_banner -loglevel error -i underwater.png -q:v 2 -vf scale=140:-1 compressed.jpeg"); // compress
+    system("ffmpeg.exe -hide_banner -loglevel error -i underwater.png -q:v 2 -vf scale=360:-1 compressed.jpeg"); // compress
     system("img2bin.exe compressed.jpeg");                                                                       // convert to bits
     printf("Image converted to bits.\n");
     return;
 }
 
-void makeAudio(int16_t *buf, long long int N)
+void makeAudio(int16_t *buf, long int N)
 {
     // Pipe the audio data to ffmpeg, which writes it to a wav file
     FILE *pipeout = popen("ffmpeg.exe -hide_banner -loglevel error -y -f s16le -acodec pcm_s16le -vn -ar 88000 -ac 1 -i - out.flac", "w");
@@ -100,12 +100,14 @@ void makeAudio(int16_t *buf, long long int N)
     pclose(pipeout);
 }
 
-void makeAudioBuffer(int16_t *buf, char *input, long long int filelen)
+void makeAudioBuffer(int16_t *buf, char *input, long int filelen)
 {
-    long long int n = 0; // buffer index
-    int j = 0;           // bit array index
-    double p2sf = 2.0 * M_PI / sf;
+    long int n = 0; // buffer index
+    int j = 0;      // bit array index
+
     int e;
+    int16_t *samples[10];
+    calcSamples(samples);
 
     printf("Making audio buffer...");
     while (j < filelen) // loop - input
@@ -116,56 +118,56 @@ void makeAudioBuffer(int16_t *buf, char *input, long long int filelen)
         {
             for (n = n; n < e; n++) // loop - audio buffer
             {
-                buf[n] = calcSample(n, 9, p2sf);
+                buf[n] = samples[9][n % bd];
             }
         }
         if (input[j] - '0' == 0 && input[j + 1] - '0' == 1 && input[j + 2] - '0' == 1) // 011
         {
             for (n = n; n < e; n++) // loop - audio buffer
             {
-                buf[n] = calcSample(n, 2, p2sf);
+                buf[n] = samples[8][n % bd];
             }
         }
         if (input[j] - '0' == 1 && input[j + 1] - '0' == 0 && input[j + 2] - '0' == 1) // 101
         {
             for (n = n; n < e; n++) // loop - audio buffer
             {
-                buf[n] = calcSample(n, 3, p2sf);
+                buf[n] = samples[7][n % bd];
             }
         }
         if (input[j] - '0' == 1 && input[j + 1] - '0' == 1 && input[j + 2] - '0' == 0) // 110
         {
             for (n = n; n < e; n++) // loop - audio buffer
             {
-                buf[n] = calcSample(n, 4, p2sf);
+                buf[n] = samples[6][n % bd];
             }
         }
         if (input[j] - '0' == 0 && input[j + 1] - '0' == 0 && input[j + 2] - '0' == 1) // 001
         {
             for (n = n; n < e; n++) // loop - audio buffer
             {
-                buf[n] = calcSample(n, 5, p2sf);
+                buf[n] = samples[5][n % bd];
             }
         }
         if (input[j] - '0' == 1 && input[j + 1] - '0' == 0 && input[j + 2] - '0' == 0) // 100
         {
             for (n = n; n < e; n++) // loop - audio buffer
             {
-                buf[n] = calcSample(n, 6, p2sf);
+                buf[n] = samples[4][n % bd];
             }
         }
         if (input[j] - '0' == 0 && input[j + 1] - '0' == 0 && input[j + 2] - '0' == 0) // 000
         {
             for (n = n; n < e; n++) // loop - audio buffer
             {
-                buf[n] = calcSample(n, 7, p2sf);
+                buf[n] = samples[3][n % bd];
             }
         }
         if (input[j] - '0' == 0 && input[j + 1] - '0' == 1 && input[j + 2] - '0' == 0) // 010
         {
             for (n = n; n < e; n++) // loop - audio buffer
             {
-                buf[n] = calcSample(n, 8, p2sf);
+                buf[n] = samples[2][n % bd];
             }
         }
         else //if something is wrong just skip this character
@@ -179,17 +181,26 @@ void makeAudioBuffer(int16_t *buf, char *input, long long int filelen)
     return;
 }
 
-void printInfo(long long int N, int filelen)
+void printInfo(long int N, int filelen)
 {
     float seconds = N / sf;
     float bps = filelen / seconds;
     printf("Input len: %d\n", filelen);
-    printf("N: %lld\n", N);
+    printf("N: %ld\n", N);
     printf("Seconds: %.0f\n", seconds);
     printf("bps: %.0f\n", bps);
 }
 
-int16_t calcSample(int n, int sub, double p2dsf)
+void calcSamples(int16_t **samples)
 {
-    return 16383.0 * sin(n * (freq * sub) * p2dsf);
+    double p2sf = 2.0 * M_PI / sf;
+    for (int i = 1; i <= 9; i++)
+    {
+        samples[i] = malloc(bd * sizeof(int16_t));
+        for (int j = 0; j < bd; j++)
+        {
+            samples[i][j] = 16383.0 * sin(j * (freq * i) * p2sf);
+        }
+    }
+    return;
 }
