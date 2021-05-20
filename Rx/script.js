dafc72a8e342faac1cde9a2fcc84145de5edec70
jhbengtsson <felsdp00@gmail.com>
2021-05-20 20:06:30 +0200

    comments

--- Rx/script.js
+++ Rx/script.js
@@ -63,32 +63,34 @@ document.getElementById("startpause").addEventListener("click", () => {
     let seperator = false;
     console.log(spec);
     while (!((compare(spec[x], 400, 480) && compare(spec[x + 1], 400, 480))) && x < spec.length) { //sort out background noise before signal has started
+      //while the frequency we are looking at, and the next frequency aren't just noise, skip and continue
       x++;
     }
     console.log("x: ", x);
-    while (x < spec.length) {
+    while (x < spec.length) { 
       while (!isValidFreq(spec[x]) && x < spec.length && !compare(spec[x], 400, 480)) {
         x++;
         if (!isValidFreq(spec[x]) && x < spec.length && !compare(spec[x], 400, 480)) {
           //x = spec.length; //if x and x+1 is not valid, skip all.
-        }
+        } //hvad sker der her, hvorfor er der både while og if haha
       }
-      while (compare(spec[x], 400, 480) && x < spec.length) { //kan det fjernes og at der laves en else istedet?
-        seperator = true;
+      while (compare(spec[x], 400, 480) && x < spec.length) { 
+        seperator = true; //while spec[x] is between 400 and 480 then it is a seperatortone
         x++;
         console.log(x);
       }
       while (!seperator && compare(spec[x], current - 40, current + 40) && x < spec.length) {
+        //hvad sker der her
         x++;
       }
 
       current = spec[x];
-      if (isValidFreq(spec[x]) && seperator) {
+      if (isValidFreq(spec[x]) && seperator) { //if the freq is valid an the seperator tone has been there
         seperator = false;
-        identifiedFreqs.push(isValidFreq(spec[x]));
+        identifiedFreqs.push(isValidFreq(spec[x])); //push to identified freqs
         bitstring += freqToBits(identifiedFreqs[identifiedFreqs.length - 1]);
         console.log("x: " + x + "freq: " + identifiedFreqs[identifiedFreqs.length-1]);
-        while (!compare(spec[x], 400, 480) && x < spec.length) {
+        while (!compare(spec[x], 400, 480) && x < spec.length) { //hvad sker der her
           x++;
         }
       }
